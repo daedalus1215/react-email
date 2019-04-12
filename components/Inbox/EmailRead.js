e58edcfe94ae67b7ea81a19ae8495456bd3cbdcd
larry <ladams1776@yahoo.com>
2019-04-12 12:13:35 -0400

    ReadEmail breaks ups the body into paragraphs & error page if the email id is not correct.

--- components/Inbox/EmailRead.js
+++ components/Inbox/EmailRead.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 class EmailRead extends React.Component {
     render() {
         const emailId = this.props.match.params.id;
-        const email = this.props.emails.filter(email => { 
-            if ( email.id === emailId ) {
-                return email;                
+        const email = this.props.emails.filter(email => {
+            if (email.id === emailId) {
+                return email;
             }
         })[0];
+
+        if (!email) {
+            return <div>
+                <h1>Error finding email</h1>
+
+                <p>
+                    Invalid email id. <Link to="/"> Go back to inbox </Link>
+                </p>
+            </div>
+        }
 
         return <div>
             <h1>{email.subject}</h1>
             <h3>{email.date} {' '} {email.email}</h3>
-            <p>{email.body}</p>
+            {email.body.split('\n\n').map((paragraphText, index) => {
+                return <p key={index}>{paragraphText}</p>
+            })}
         </div>
     }
 }
